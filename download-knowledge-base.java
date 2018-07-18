7248235d18af9aed97e928a37d568cb65dc77703
Winona Azure <38537084+wiazur@users.noreply.github.com>
2018-07-18 16:12:58 -0700

    Initial commit

---
+++ download-knowledge-base.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.lang.reflect.Type;
+import java.net.*;
+import java.util.*;
+import javax.net.ssl.HttpsURLConnection;
+
+/*
+ * Gson: https://github.com/google/gson
+ * Maven info:
+ *    <dependency>
+ *      <groupId>com.google.code.gson</groupId>
+ *      <artifactId>gson</artifactId>
+ *      <version>2.8.1</version>
+ *    </dependency>
+ */
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+/* NOTE: To compile and run this code:
+1. Save this file as GetQnA.java.
+2. Run:
+    javac GetQnA.java -cp .;gson-2.8.1.jar -encoding UTF-8
+3. Run:
+    java -cp .;gson-2.8.1.jar GetQnA
+*/
+
+public class GetQnA {
+
+    // **********************************************
+    // *** Update or verify the following values. ***
+    // **********************************************
+
+    // Replace this with a valid subscription key.
+    static String subscriptionKey = "ADD KEY HERE";
+
+    // Replace this with a valid knowledge base ID.
+    static String kb = "ADD ID HERE";
+
+    // Replace this with "test" or "prod".
+    static String env = "test";
+
+    // Represents the various elements used to create HTTP request URIs
+    // for QnA Maker operations.
+    static String host = "https://westus.api.cognitive.microsoft.com";
+    static String service = "/qnamaker/v4.0";
+    static String method = "/knowledgebases/%s/%s/qna/";
+
+    /**
+     * Formats and indents JSON for display.
+     * @param json_text The raw JSON input
+     * @return A string containing formatted and indented JSON.
+     */
+    public static String PrettyPrint (String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(json_text);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    /**
+     * Sends a GET HTTP request.
+     * @param url The string URL request
+     * @return A Response object with the header and body of the response
+     */
+    public static String Get (URL url) throws Exception {
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+        connection.setDoOutput(true);
+
+        StringBuilder response = new StringBuilder ();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+        in.close();
+
+        return response.toString();
+    }
+
+    /** 
+     * Compiles URL to send the request to download the knowledge base.
+     * @param kb The knowledge base ID
+     * @return The downloaded knowledge base
+     */
+    public static String GetQnA (String kb) throws Exception {
+        String method_with_id = String.format (method, kb, env);
+        URL url = new URL (host + service + method_with_id);
+        System.out.println ("Calling " + url.toString() + ".");
+        return Get(url);
+    }
+
+    public static void main(String[] args) {
+        try {
+            String response = GetQnA (kb);
+            System.out.println (PrettyPrint (response));
+        }
+        catch (Exception e) {
+            System.out.println (e.getCause().getMessage());
+        }
+    }
+}
