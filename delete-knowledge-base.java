0837748c5d0b1f0861449edbda32b46f42d19635
Winona Azure <38537084+wiazur@users.noreply.github.com>
2018-09-11 21:09:55 -0700

    Added delete KB sample

---
+++ delete-knowledge-base.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.lang.reflect.Type;
+import java.net.*;
+import java.util.*;
+import javax.net.ssl.HttpsURLConnection;
+
+/*
+ * Gson: https://github.com/google/gson
+ * Maven info:
+ *    <dependency>
+ *      <groupId>com.google.code.gson</groupId>
+ *      <artifactId>gson</artifactId>
+ *      <version>2.8.1</version>
+ *    </dependency>
+ */
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+/* NOTE: To compile and run this code:
+1. Save this file as DeleteKB.java.
+2. Run:
+    javac DeleteKB.java -cp .;gson-2.8.1.jar -encoding UTF-8
+3. Run:
+    java -cp .;gson-2.8.1.jar DeleteKB
+*/
+
+public class DeleteKB {
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+    // Replace this with a valid subscription key.
+    static String subscriptionKey = "ADD KEY HERE";
+
+    // Replace this with a valid knowledge base ID, the one you want to delete.
+    static String kb = "ADD KNOWLEDGE BASE ID HERE";
+
+    static String host = "https://westus.api.cognitive.microsoft.com";
+    static String service = "/qnamaker/v4.0";
+    static String method = "/knowledgebases/";
+
+    public static String PrettyPrint (String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(json_text);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    // Send an HTTP DELETE request.
+    public static String Delete (URL url) throws Exception {
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("DELETE");
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+        connection.setDoOutput(true);
+
+        if (connection.getResponseCode() == 204)
+        {
+            return "{'result' : 'Success.'}";
+        }
+        else {
+            StringBuilder response = new StringBuilder ();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getErrorStream(), "UTF-8"));
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            in.close();
+
+            return response.toString();
+        }
+    }
+
+    // Sends the request to delete the knowledge base.
+    public static String DeleteKB(String kb) throws Exception {
+        URL url = new URL (host + service + method + kb);
+        System.out.println ("Calling " + url.toString() + ".");
+        return Delete(url);
+    }
+
+    public static void main(String[] args) {
+        try {
+            String response = DeleteKB (kb);
+            System.out.println (PrettyPrint (response));
+        }
+        catch (Exception e) {
+            System.out.println (e.getCause().getMessage());
+        }
+    }
+}
