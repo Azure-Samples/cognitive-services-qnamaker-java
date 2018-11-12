8ceab7ed9f5e0a96ea5e41a64fda5d7b7b7dbf9c
Dina Berry <41597107+diberry@users.noreply.github.com>
2018-11-12 06:17:12 -0800

    get answer final

--- documentation-samples/quickstarts/get-answer/GetAnswer.java
+++ documentation-samples/quickstarts/get-answer/GetAnswer.java
@@ -12,6 +12,10 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.apache.http.entity.StringEntity;
 
+// 1. Replace variable values with your own from QnA Maker Publish page
+// 2. Compile with: javac -cp "lib/*" GetAnswer.java
+// 3. Execute with: java -cp ".;lib/*" GetAnswer
+
 public class GetAnswer {
 
     public static void main(String[] args) 
@@ -22,17 +26,17 @@ public class GetAnswer {
             // Represents the various elements used to create HTTP request URIs
             // for QnA Maker operations.
             // From Publish Page: HOST
-            // Example: https://diberry-qnamaker-f0.azurewebsites.net/qnamaker
+            // Example: https://YOUR-RESOURCE-NAME.azurewebsites.net/qnamaker
             String host = "YOUR-KB-HOST";
 
             // Management APIs postpend the version to the route
             // From Publish Page, value after POST
             // Example: /knowledgebases/ZZZ15f8c-d01b-4698-a2de-85b0dbf3358c/generateAnswer
-            String service = "YOUR-SERVICE-ROUTE";
+            String service = "YOUR-KB-ROUTE";
 
             // Authorization endpoint key
             // From Publish Page
-            String endpointKey = "EndpointKey YOUR-ENDPOINT-KEY";
+            String endpointKey = "EndpointKey YOUR-KB-ENDPOINT";
 
             // JSON format for passing question to service
             String question = "{ 'question' : 'Is the QnA Maker Service free?', 'top' : 3 }";
@@ -53,7 +57,7 @@ public class GetAnswer {
             request.setHeader("Content-type", "application/json");
 
             // set authorization
-            request.setHeader("Authorization",authorizationHeader);
+            request.setHeader("Authorization",endpointKey);
             System.out.println("authorizationHeader " + endpointKey);
 
             // Send request to Azure service, get response
