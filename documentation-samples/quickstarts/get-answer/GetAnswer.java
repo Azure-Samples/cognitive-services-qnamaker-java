f454c2f5f6f7d622f448bb327b0424832fabcdb1
Dina Berry <41597107+diberry@users.noreply.github.com>
2018-11-18 13:11:47 -0800

    revert getanswer

--- documentation-samples/quickstarts/get-answer/GetAnswer.java
+++ documentation-samples/quickstarts/get-answer/GetAnswer.java
@@ -23,19 +23,42 @@ public class GetAnswer {
 
         try
         {
-            String knowledge_base_id = "YOUR-KNOWLEDGE-BASE-ID";
-            String resource_key = "YOUR-RESOURCE-KEY";
+            // Represents the various elements used to create HTTP request URIs
+            // for QnA Maker operations.
+            // From Publish Page: HOST
+            // Example: https://YOUR-RESOURCE-NAME.azurewebsites.net/qnamaker
+            String host = "YOUR-KB-HOST";
 
-            String host = String.Format("https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/knowledgebases/%s", knowledge_base_id);
+            // Management APIs postpend the version to the route
+            // From Publish Page, value after POST
+            // Example: /knowledgebases/ZZZ15f8c-d01b-4698-a2de-85b0dbf3358c/generateAnswer
+            String service = "YOUR-KB-ROUTE";
 
+            // Authorization endpoint key
+            // From Publish Page
+            String endpointKey = "EndpointKey YOUR-KB-ENDPOINT";
+
+            // JSON format for passing question to service
+            String question = "{ 'question' : 'Is the QnA Maker Service free?', 'top' : 3 }";
 
             // Create http client
             HttpClient httpclient = HttpClients.createDefault();
 
-            HttpPost request = new HttpPost(host);
+            // Add host + service to get full URI
+            String answer_uri = host + service;
+            System.out.println(answer_uri);
+
+            //
+            HttpPost request = new HttpPost(answer_uri);
+
+            // set question
+            StringEntity entity = new StringEntity(question);
+            request.setEntity(entity);
+            request.setHeader("Content-type", "application/json");
 
             // set authorization
-            request.setHeader("Ocp-Apim-Subscription-Key",resource_key);
+            request.setHeader("Authorization",endpointKey);
+            System.out.println("authorizationHeader " + endpointKey);
 
             // Send request to Azure service, get response
             HttpResponse response = httpclient.execute(request);
